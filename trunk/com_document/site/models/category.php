d0fbb73e5984b6a215a38f410ec3185a727ae069
chdemko <chdemko@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-08 10:36:29 +0000

    Adding category and categories models
    git-svn-id: http://joomlacode.org/svn/document@14 ba448fd9-4976-4c7a-9a71-c868f45c6528

---
+++ trunk/com_document/site/models/category.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @version		$Id$
+ * @package		Document
+ * @subpackage	Component
+ * @copyright	Copyright (C) 2010 - today Master ICONE, University of La Rochelle, France.
+ * @link		http://joomlacode.org/gf/project/document/
+ * @license		License GNU General Public License version 2 or later
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// import Joomla modelitem library
+jimport('joomla.application.component.modelitem');
+
+/**
+ * Category Model of Document component
+ */
+class DocumentModelCategory extends JModelItem
+{
+}

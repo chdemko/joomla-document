a22a63b279591be7da59bde2e21302b56d1f3d29
slimane <slimane@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-13 14:26:43 +0000

    Coding categories model in the site part
    git-svn-id: http://joomlacode.org/svn/document@17 ba448fd9-4976-4c7a-9a71-c868f45c6528

--- trunk/com_document/site/models/categories.php
+++ trunk/com_document/site/models/categories.php
@@ -20,4 +20,105 @@ jimport('joomla.application.component.modelitem');
  */
 class DocumentModelCategories extends JModelItem
 {
+
+	/**
+	 * The category context (allows other extensions to derived from this model).
+	 *
+	 * @var		string
+	 */
+	protected $extension = 'com_document';
+
+	private $_parent = null;
+
+	private $_items = null;
+
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @since	1.6
+	 */
+	protected function populateState()
+	{
+		$app = JFactory::getApplication();
+		$this->setState('filter.extension', $this->extension);
+
+		// Get the parent id if defined.
+		$parentId = JRequest::getInt('id');
+		$this->setState('filter.parentId', $parentId);
+
+		$params = $app->getParams();
+		$this->setState('params', $params);
+
+		$this->setState('filter.published',	1);
+		$this->setState('filter.access',	true);
+	}
+
+	/**
+	 * Method to get a store id based on model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param	string		$id	A prefix for the store id.
+	 *
+	 * @return	string		A store id.
+	 */
+	protected function getStoreId($id = '')
+	{
+		// Compile the store id.
+		$id	.= ':'.$this->getState('filter.extension');
+		$id	.= ':'.$this->getState('filter.published');
+		$id	.= ':'.$this->getState('filter.access');
+		$id	.= ':'.$this->getState('filter.parentId');
+
+		return parent::getStoreId($id);
+	}
+
+	/**
+	 * Redefine the function an add some properties to make the styling more easy
+	 *
+	 * @param	bool	$recursive	True if you want to return children recursively.
+	 *
+	 * @return	mixed	An array of data items on success, false on failure.
+	 * @since	1.6
+	 */
+	public function getItems($recursive = false)
+	{
+		if (!count($this->_items)) {
+			$app = JFactory::getApplication();
+			$menu = $app->getMenu();
+			$active = $menu->getActive();
+			$params = new JRegistry();
+
+			if ($active) {
+				$params->loadJSON($active->params);
+			}
+
+			$options = array();
+			$options['countItems'] = $params->get('show_cat_num_articles_cat', 1) || !$params->get('show_empty_categories_cat', 0);
+			$categories = JCategories::getInstance('Document', $options);
+			$this->_parent = $categories->get($this->getState('filter.parentId', 'root'));
+
+			if (is_object($this->_parent)) {
+				$this->_items = $this->_parent->getChildren($recursive);
+			}
+			else {
+				$this->_items = false;
+			}
+		}
+
+		return $this->_items;
+	}
+
+	public function getParent()
+	{
+		if (!is_object($this->_parent)) {
+			$this->getItems();
+		}
+
+		return $this->_parent;
+	}
 }

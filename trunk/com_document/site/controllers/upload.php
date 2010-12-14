2a02a253ee2ca62cfe37b1ca7df9418b772b3bea
eexit <eexit@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-14 14:30:56 +0000

    First commit for site/controllers/upload.php
    git-svn-id: http://joomlacode.org/svn/document@73 ba448fd9-4976-4c7a-9a71-c868f45c6528

--- trunk/com_document/site/controllers/upload.php
+++ trunk/com_document/site/controllers/upload.php
@@ -20,4 +20,59 @@ jimport('joomla.application.component.controller');
  */
 class DocumentControllerUpload extends JController
 {
+    /**
+	 * @since	1.6
+	 */
+	protected $view_item = 'form';
+
+	/**
+	 * @since	1.6
+	 */
+	protected $view_list = 'categories';
+
+	/**
+	 * Constructor
+	 *
+	 * @since	1.6
+	 */
+	public function __construct($config = array())
+	{
+		parent::__construct($config);
+	}
+	
+    /**
+	 * Method to add a new record.
+	 *
+	 * @return	boolean	True if the article can be added, false if not.
+	 * @since	1.6
+	 */
+	public function add()
+	{
+		$app		= JFactory::getApplication();
+		$context	= "$this->option.edit.$this->context";
+
+		// Access check
+		if (!$this->allowAdd()) {
+			JError::raiseError(403, JText::_('JERROR_ALERTNOAUTHOR'));
+
+			return false;
+		}
+
+		// Clear the record edit information from the session.
+		$app->setUserState($context.'.data',	null);
+
+		// Clear the return page.
+		// TODO: We should be including an optional 'return' variable in the URL.
+		$this->setReturnPage();
+
+		// ItemID required on redirect for correct Template Style
+		$redirect = 'index.php?option=com_document&view=form&layout=edit';
+		if (JRequest::getInt('Itemid') != 0) {
+			$redirect .= '&Itemid='.JRequest::getInt('Itemid');
+		}
+
+		$this->setRedirect($redirect);
+
+		return true;
+	}
 }

b891ae25f74e34325f062eab12552be57b01de6c
raggad <raggad@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-14 09:27:10 +0000

    update document.php
    git-svn-id: http://joomlacode.org/svn/document@29 ba448fd9-4976-4c7a-9a71-c868f45c6528

--- trunk/com_document/admin/document.php
+++ trunk/com_document/admin/document.php
@@ -11,4 +11,23 @@
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
+// Access check.
+if (!JFactory::getUser()->authorise('core.manage', 'com_document')) 
+{
+	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+}
 
+// require helper file
+JLoader::register('documentHelper', dirname(__FILE__) . DS . 'helpers' . DS . 'document.php');
+
+// import joomla controller library
+jimport('joomla.application.component.controller');
+
+// Get an instance of the controller prefixed by document
+$controller = JController::getInstance('document');
+
+// Perform the Request task
+$controller->execute(JRequest::getCmd('task'));
+
+// Redirect if set by the controller
+$controller->redirect();

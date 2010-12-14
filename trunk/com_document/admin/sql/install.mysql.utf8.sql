5c6222dfd906872673afadecf414495cc71cdcab
raggad <raggad@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-14 12:29:30 +0000

    update instal.sql
    git-svn-id: http://joomlacode.org/svn/document@47 ba448fd9-4976-4c7a-9a71-c868f45c6528

--- trunk/com_document/admin/sql/install.mysql.utf8.sql
+++ trunk/com_document/admin/sql/install.mysql.utf8.sql
@@ -1,33 +1,33 @@
 -- $Id$
 
 DROP TABLE IF EXISTS `#__document`;
- 
-CREATE TABLE `#__document` (
-  `id` int() NOT NULL AUTO_INCREMENT,
-  `title`  varchar(255) NOT NULL,
-  `keywords`  varchar(255) NOT NULL,
-  `description`	 varchar(255) NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `alias`  varchar(255) NOT NULL,
-  `filename` varchar(255) NOT NULL,
-  `mime`  varchar(255) NOT NULL,
-  `catid` int()NOT NULL,
+  
+CREATE TABLE IF NOT EXISTS `#__document` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title`  varchar(255) NOT NULL  default '',
+  `keywords`  varchar(255) NOT NULL default '',
+  `description`	 varchar(255) NOT NULL default '',
+  `author` varchar(255) NOT NULL default '',
+  `alias`  varchar(255) NOT NULL default '',
+  `filename` varchar(255) NOT NULL default '',
+  `mime`  varchar(255) NOT NULL default '',
+  `catid` int NOT NULL default'0',
   `created` date,
-  `created_by`int()NOT NULL,
-  `created_by_alias` varchar(25) NOT NULL,
-  `modified` date,
-  `modified_by` int()NOT NULL,
-  `hits` int()NOT NULL,
+  `created_by` int NOT NULL default'0',
+  `created_by_alias` varchar(255) NOT NULL,
+  `modified` date default '0000-00-00 00:00:00' ,
+  `modified_by` int NOT NULL default'0',
+  `hits` int NOT NULL default'0',
   `params` varchar(65535) ,
-  `language` int() NOT NULL,
+  `language` char(7) NOT NULL DEFAULT '',
   `featured` tinyint(3),
-  `ordering` int()NOT NULL,
-  `published` int()NOT NULL,
+  `ordering` int NOT NULL,
+  `published` int NOT NULL default'0',
   `published_up` date,
   `published_down` date,
-  `access` int()NOT NULL,
-  `checked_out` int()NOT NULL,
-  `checked_out_time` date,
+  `access` int NOT NULL default'0',
+  `checked_out` int NOT NULL default'0',
+  `checked_out_time` date default '0000-00-00 00:00:00',
      PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
  

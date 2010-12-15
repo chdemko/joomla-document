66791cea9d060ca709db91c8655f21b08db52e1c
tbonnaud <tbonnaud@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-15 12:01:54 +0000

    git-svn-id: http://joomlacode.org/svn/document@94 ba448fd9-4976-4c7a-9a71-c868f45c6528

--- trunk/com_document/admin/views/download/view.raw.php
+++ trunk/com_document/admin/views/download/view.raw.php
@@ -14,6 +14,7 @@ defined('_JEXEC') or die('Restricted access');
 
 // import Joomla view library
 jimport('joomla.application.component.view');
+jimport('joomla.filesystem.file');
 
 /**
  * Download View of Document component
@@ -25,7 +26,25 @@ class DocumentViewDownload extends JView
 	 */
 	function display($tpl = null) 
 	{
-		// Display the template
-		parent::display($tpl);
+		$item	= $this->get('item');
+		
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			JError::raiseError(500, implode("<br />", $errors));
+			return false;
+		}
+		if (!in_array($item->access,JFactory::getUser()->getAuthorisedViewLevels()) {
+			JError::raiseError(403, JText::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN'));
+			return false;
+		}
+		if(!JFile::exists($item->filename){
+			JError::raiseError(404, JText::_('COM_DOCUMENT_ERROR_DOCUMENT_NOT_FOUND'));
+			return false;
+		}
+		
+		$document = JFactory::getDocument();
+		$document->setMimeEncoding($item->mime);
+		JResponse::setHeader('Content-disposition', 'attachment; filename="'.$item->title.'.'.JFile::getExt($item->filename).'"; creation-date="'.JFactory::getDate()->toRFC822().'"', true);
+		echo file_get_contents(JPATH_ROOT . '/' . $item->filename);
 	}
 }

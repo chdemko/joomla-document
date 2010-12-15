107eb2c16ba698c3bd2134e86ad7c628c7cd1213
tbonnaud <tbonnaud@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-15 12:01:44 +0000

    git-svn-id: http://joomlacode.org/svn/document@93 ba448fd9-4976-4c7a-9a71-c868f45c6528

--- trunk/com_document/admin/views/document/view.html.php
+++ trunk/com_document/admin/views/document/view.html.php
@@ -20,12 +20,92 @@ jimport('joomla.application.component.view');
  */
 class DocumentViewDocument extends JView
 {
+	protected $form;
+	protected $item;
+	protected $state;
 	/**
 	 * Document view display method
 	 */
 	public function display($tpl = null) 
 	{
-		// Display the template
+		if ($this->getLayout() == 'pagebreak') {
+			// TODO: This is really dogy - should change this one day.
+			$eName		= JRequest::getVar('e_name');
+			$eName		= preg_replace( '#[^A-Z0-9\-\_\[\]]#i', '', $eName );
+			$document	= JFactory::getDocument();
+			$document->setTitle(JText::_('COM_DOCUMENT_PAGEBREAK_DOC_TITLE'));
+			$this->assignRef('eName', $eName);
+			parent::display($tpl);
+			return;
+		}
+		
+		// Initialiase variables.
+		$this->form		= $this->get('Form');
+		$this->item		= $this->get('item');
+		$this->state	= $this->get('State');
+		$this->canDo	= documentHelper::getActions($this->state->get('filter.category_id'));
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			JError::raiseError(500, implode("\n", $errors));
+			return false;
+		}
+		var_dump($this->item);
+		$this->addToolbar();
 		parent::display($tpl);
+		}
+		/**
+	 * Add the page title and toolbar.
+	 *
+	 * @since	1.6
+	 */
+	protected function addToolbar()
+	{
+		JRequest::setVar('hidemainmenu', true);
+		
+		$user		= JFactory::getUser();
+		$userId		= $user->get('id');
+		$isNew		= ($this->item->id == 0);
+		$checkedOut	= !($this->item->checked_out == 0 || $this->item->checked_out == $userId);
+		$canDo		= documentHelper::getActions($this->state->get('filter.category_id'), $this->item->id);
+
+		JToolBarHelper::title(JText::_('COM_DOCUMENT_PAGE_'.($checkedOut ? 'VIEW_DOCUMENT' : ($isNew ? 'ADD_DOCUMENT' : 'EDIT_DOCUMENT'))), 'document-add.png');
+
+		// Built the actions for new and existing records.
+		if ($isNew)  {
+			// For new records, check the create permission.
+			if ($canDo->get('core.create')) {
+				JToolBarHelper::apply('document.apply', 'JTOOLBAR_APPLY');
+				JToolBarHelper::save('document.save', 'JTOOLBAR_SAVE');
+				JToolBarHelper::custom('document.save2new', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
+			}
+
+			JToolBarHelper::cancel('document.cancel', 'JTOOLBAR_CANCEL');
+		}
+		else {
+			// Can't save the record if it's checked out.
+			if (!$checkedOut) {
+				// Since it's an existing record, check the edit permission, or fall back to edit own if the owner.
+				if ($canDo->get('core.edit') || ($canDo->get('core.edit.own') && $this->item->created_by == $userId)) {
+					JToolBarHelper::apply('document.apply', 'JTOOLBAR_APPLY');
+					JToolBarHelper::save('document.save', 'JTOOLBAR_SAVE');
+
+					// We can save this record, but check the create permission to see if we can return to make a new one.
+					if ($canDo->get('core.create')) {
+						JToolBarHelper::custom('document.save2new', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
+					}
+				}
+			}
+
+			// If checked out, we can still save
+			if ($canDo->get('core.create')) {
+				JToolBarHelper::custom('document.save2copy', 'save-copy.png', 'save-copy_f2.png', 'JTOOLBAR_SAVE_AS_COPY', false);
+			}
+
+			JToolBarHelper::cancel('document.cancel', 'JTOOLBAR_CLOSE');
+		}
+
+		JToolBarHelper::divider();
+		JToolBarHelper::help('JHELP_DOCUMENT_DOCUMENT_MANAGER_EDIT');
 	}
+	
 }

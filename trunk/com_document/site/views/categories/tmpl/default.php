fee9c7cb4e7aff7f78d993204dcf2db9e3ef6241
moting <moting@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-17 12:25:24 +0000

    git-svn-id: http://joomlacode.org/svn/document@140 ba448fd9-4976-4c7a-9a71-c868f45c6528

--- trunk/com_document/site/views/categories/tmpl/default.php
+++ trunk/com_document/site/views/categories/tmpl/default.php
@@ -1,14 +1,40 @@
 <?php
-
 /**
  * @version		$Id$
- * @package		Document
- * @subpackage	Component
- * @copyright	Copyright (C) 2010 - today Master ICONE, University of La Rochelle, France.
- * @link		http://joomlacode.org/gf/project/document/
- * @license		http://www.gnu.org/licenses/gpl-2.0.html
+ * @package		Joomla.Site
+ * @subpackage	com_Document
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+// no direct access
+defined('_JEXEC') or die;
 
+JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
+
+// If the page class is defined, wrap the whole output in a div.
+$pageClass = $this->params->get('pageclass_sfx');
+?>
+<div class="categories-list<?php echo $pageClass;?>">
+<?php if ($this->params->get('show_page_heading', 1)) : ?>
+<h1>
+	<?php echo $this->escape($this->params->get('page_heading')); ?>
+</h1>
+<?php endif; ?>
+<?php if ($this->params->get('show_base_description')) : ?>
+	<?php 	//If there is a description in the menu parameters use that; ?>
+		<?php if($this->params->get('categories_description')) : ?>
+			<?php echo  JHtml::_('document.prepare',$this->params->get('categories_description')); ?>
+		<?php  else: ?>
+			<?php //Otherwise get one from the database if it exists. ?>
+			<?php  if ($this->parent->description) : ?>
+				<div class="category-desc">
+					<?php  echo JHtml::_('document.prepare', $this->parent->description); ?>
+				</div>
+			<?php  endif; ?>
+		<?php  endif; ?>
+	<?php endif; ?>
+<?php
+echo $this->loadTemplate('items');
+?>
+</div>

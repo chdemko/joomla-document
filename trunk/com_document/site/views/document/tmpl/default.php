3fb534c411cc016f7f116507d694367b295888d0
hammihicham <hammihicham@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-17 08:07:09 +0000

    git-svn-id: http://joomlacode.org/svn/document@121 ba448fd9-4976-4c7a-9a71-c868f45c6528

--- trunk/com_document/site/views/document/tmpl/default.php
+++ trunk/com_document/site/views/document/tmpl/default.php
@@ -1,14 +1,40 @@
 <?php
-
 /**
  * @version		$Id$
- * @package		Document
- * @subpackage	Component
- * @copyright	Copyright (C) 2010 - today Master ICONE, University of La Rochelle, France.
- * @link		http://joomlacode.org/gf/project/document/
- * @license		http://www.gnu.org/licenses/gpl-2.0.html
+ * @package		Joomla.Site
+ * @subpackage	com_content
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+// no direct access
+defined('_JEXEC') or die;
+?>
+<H1>Document vue</H1>
+
+<div>
+        <strong>title:</strong>
+    </div>
+    <p>
+       <?php echo $this->item->title  ?>   </p>
+
+<div><strong>Author:</strong></div>
+    <p>
+        <?php echo $this->item->category?> 
+    </p>
+    <div><strong>categorie:</strong></div>
+    <p>
+        <?php echo $this->item->category ?> 
+    </p>
+
+  <div><strong>language:</strong></div>
+    <p> 
+      <?php  echo $this->item->language  ?>
+    </p>   
+	
+	
+<a href="<?php echo JRoute_('index.php?option=com_document&view=download&id=.$this->item->id')?>">Download here</a>
+ 
+
+
 

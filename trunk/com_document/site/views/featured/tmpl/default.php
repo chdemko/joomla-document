20c9f56fe0638dda9c9f303f65c8ffb2d6494af2
hammihicham <hammihicham@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-17 12:41:16 +0000

    git-svn-id: http://joomlacode.org/svn/document@145 ba448fd9-4976-4c7a-9a71-c868f45c6528

--- trunk/com_document/site/views/featured/tmpl/default.php
+++ trunk/com_document/site/views/featured/tmpl/default.php
@@ -1,14 +1,59 @@
 <?php
-
 /**
  * @version		$Id$
- * @package		Document
- * @subpackage	Component
- * @copyright	Copyright (C) 2010 - today Master ICONE, University of La Rochelle, France.
- * @link		http://joomlacode.org/gf/project/document/
- * @license		http://www.gnu.org/licenses/gpl-2.0.html
+ * @package		Joomla.Site
+ * @subpackage	com_document
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+// no direct access
+defined('_JEXEC') or die;
 
+JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
+
+$pageClass = $this->params->get('pageclass_sfx');
+?>
+<div class="category-list <?php echo $pageClass;?>">
+
+	<?php if ($this->params->get('show_page_heading', 1)) : ?>
+	<h1>
+		<?php echo $this->escape($this->params->get('page_heading')); ?>
+	</h1>
+	<?php endif; ?>
+
+	<?php if ($this->params->get('show_category_title', 1) OR $this->params->get('page_subheading')) : ?>
+	<h2>
+		<?php echo $this->escape($this->params->get('page_subheading')); ?>
+		<?php if ($this->params->get('show_category_title')) : ?>
+			<span class="subheading-category"><?php echo $this->category->title;?></span>
+		<?php endif; ?>
+	</h2>
+	<?php endif; ?>
+
+	<?php if ($this->params->get('show_description', 1) || $this->params->def('show_description_image', 1)) : ?>
+	<div class="category-desc">
+		<?php if ($this->params->get('show_description_image') && $this->category->getParams()->get('image')) : ?>
+			<img src="<?php echo $this->category->getParams()->get('image'); ?>"/>
+		<?php endif; ?>
+		<?php if ($this->params->get('show_description') && $this->category->description) : ?>
+			<?php echo JHtml::_('document.prepare', $this->category->description); ?>
+		<?php endif; ?>
+		<div class="clr"></div>
+	</div>
+	<?php endif; ?>
+
+	<div class="cat-items">
+		<?php echo $this->loadTemplate('documents'); ?>
+	</div>
+
+	<?php if (!empty($this->children[$this->category->id])&& $this->maxLevel != 0) : ?>
+	<div class="cat-children">
+		<h3>
+			<?php echo JTEXT::_('JGLOBAL_SUBCATEGORIES'); ?>
+		</h3>
+
+		<?php echo $this->loadTemplate('children'); ?>
+	</div>
+	<?php endif; ?>
+</div>
--- trunk/com_document/site/views/featured/view.html.php
+++ trunk/com_document/site/views/featured/view.html.php
@@ -1,31 +1,175 @@
 <?php
-
 /**
  * @version		$Id$
- * @package		Document
- * @subpackage	Component
- * @copyright	Copyright (C) 2010 - today Master ICONE, University of La Rochelle, France.
- * @link		http://joomlacode.org/gf/project/document/
- * @license		http://www.gnu.org/licenses/gpl-2.0.html
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+// No direct access
+defined('_JEXEC') or die;
 
-// import Joomla view library
 jimport('joomla.application.component.view');
 
 /**
- * Featured View of Document component
+ * Frontpage View class
+ *
+ * @package		Joomla.Site
+ * @subpackage	com_Document
+ * @since		1.5
  */
 class DocumentViewFeatured extends JView
 {
+	protected $state = null;
+	protected $item = null;
+	protected $items = null;
+	protected $pagination = null;
+
+	protected $lead_items = array();
+	protected $intro_items = array();
+	protected $link_items = array();
+	protected $columns = 1;
+
 	/**
-	 * Featured view display method
+	 * Display the view
+	 *
+	 * @return	mixed	False on error, null otherwise.
 	 */
-	public function display($tpl = null) 
+	function display($tpl = null)
 	{
-		// Display the template
+		// Initialise variables.
+		$user = JFactory::getUser();
+		$app = JFactory::getApplication();
+
+		$state 		= $this->get('State');
+		$items 		= $this->get('Items');
+		$pagination	= $this->get('Pagination');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			JError::raiseWarning(500, implode("\n", $errors));
+			return false;
+		}
+
+		$params = &$state->params;
+
+		// PREPARE THE DATA
+
+		// Get the metrics for the structural page layout.
+		$numLeading = $params->def('num_leading_documents', 1);
+		$numIntro = $params->def('num_intro_documents', 4);
+		$numLinks = $params->def('num_links', 4);
+
+		// Compute the document slugs and prepare introtext (runs Document plugins).
+		foreach ($items as $i => & $item)
+		{
+			$item->slug = $item->alias ? ($item->id . ':' . $item->alias) : $item->id;
+			$item->catslug = ($item->category_alias) ? ($item->catid . ':' . $item->category_alias) : $item->catid;
+			$item->parent_slug = ($item->parent_alias) ? ($item->parent_id . ':' . $item->parent_alias) : $item->parent_id;
+			// No link for ROOT category
+			if ($item->parent_alias == 'root') {
+				$item->parent_slug = null;
+			}
+
+			$item->event = new stdClass();
+
+			$dispatcher = JDispatcher::getInstance();
+
+			// Ignore Document plugins on links.
+			if ($i < $numLeading + $numIntro)
+			{
+				$item->introtext = JHtml::_('document.prepare', $item->introtext);
+
+				$results = $dispatcher->trigger('onDocumentAfterTitle', array('com_document.document', &$item, &$item->params, 0));
+				$item->event->afterDisplayTitle = trim(implode("\n", $results));
+
+				$results = $dispatcher->trigger('onDocumentBeforeDisplay', array('com_document.document', &$item, &$item->params, 0));
+				$item->event->beforeDisplayDocument = trim(implode("\n", $results));
+
+				$results = $dispatcher->trigger('onDocumentAfterDisplay', array('com_document.document', &$item, &$item->params, 0));
+				$item->event->afterDisplayDocument = trim(implode("\n", $results));
+			}
+		}
+
+		// Preprocess the breakdown of leading, intro and linked DOCUMENTs.
+		// This makes it much easier for the designer to just interogate the arrays.
+		$max = count($items);
+
+		// The first group is the leading DOCUMENTs.
+		$limit = $numLeading;
+		for ($i = 0; $i < $limit && $i < $max; $i++)
+		{
+			$this->lead_items[$i] = &$items[$i];
+		}
+
+		// The second group is the intro DOCUMENTs.
+		$limit = $numLeading + $numIntro;
+		// Order DOCUMENTs across, then down (or single column mode)
+		for ($i = $numLeading; $i < $limit && $i < $max; $i++)
+		{
+			$this->intro_items[$i] = &$items[$i];
+		}
+
+		$this->columns = max(1, $params->def('num_columns', 1));
+		$order = $params->def('multi_column_order', 1);
+
+		if ($order == 0 && $this->columns > 1)
+		{
+			// call order down helper
+			$this->intro_items = DocumentHelperQuery::orderDownColumns($this->intro_items, $this->columns);
+		}
+
+		// The remainder are the links.
+		for ($i = $numLeading + $numIntro; $i < $max; $i++)
+		{
+			$this->link_items[$i] = &$items[$i];
+		}
+
+		$this->assignRef('params', $params);
+		$this->assignRef('items', $items);
+		$this->assignRef('pagination', $pagination);
+		$this->assignRef('user', $user);
+
+		$this->_prepareDocument();
+
 		parent::display($tpl);
+	}
+
+	/**
+	 * Prepares the document
+	 */
+	protected function _prepareDocument()
+	{
+		$app		= JFactory::getApplication();
+		$menus		= $app->getMenu();
+		$title 		= null;
+
+		// Because the application sets a default page title,
+		// we need to get it from the menu item itself
+		$menu = $menus->getActive();
+		if ($menu)
+		{
+			$this->params->def('page_heading', $this->params->get('page_title', $menu->title));
+		} else {
+			$this->params->def('page_heading', JText::_('JGLOBAL_DOCUMENTS'));
+		}
+
+		$title = $this->params->get('page_title', '');
+		if (empty($title)) {
+			$title = htmlspecialchars_decode($app->getCfg('sitename'));
+		}
+		elseif ($app->getCfg('sitename_pagetitles', 0)) {
+			$title = JText::sprintf('JPAGETITLE', htmlspecialchars_decode($app->getCfg('sitename')), $title);
+		}
+		$this->document->setTitle($title);
+
+		// Add feed links
+		if ($this->params->get('show_feed_link', 1))
+		{
+			$link = '&format=feed&limitstart=';
+			$attribs = array('type' => 'application/rss+xml', 'title' => 'RSS 2.0');
+			$this->document->addHeadLink(JRoute::_($link . '&type=rss'), 'alternate', 'rel', $attribs);
+			$attribs = array('type' => 'application/atom+xml', 'title' => 'Atom 1.0');
+			$this->document->addHeadLink(JRoute::_($link . '&type=atom'), 'alternate', 'rel', $attribs);
+		}
 	}
 }

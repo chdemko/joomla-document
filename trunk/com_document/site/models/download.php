b2f16f90888e5e25ca26a5718a0ca035e15ebea8
hammihicham <hammihicham@ba448fd9-4976-4c7a-9a71-c868f45c6528>
2010-12-17 13:49:30 +0000

    Model pour la vue download qui herite directement du model vue document
    git-svn-id: http://joomlacode.org/svn/document@163 ba448fd9-4976-4c7a-9a71-c868f45c6528

---
+++ trunk/com_document/site/models/download.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @version		$Id: document.php 70 2010-12-14 14:26:12Z slimane $
+ * @package		Document
+ * @subpackage	Component
+ * @copyright	Copyright (C) 2010 - today Master ICONE, University of La Rochelle, France.
+ * @link		http://joomlacode.org/gf/project/document/
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// import Joomla modelitem library
+jimport('joomla.application.component.modelitem');
+
+/**
+ * Document Model of Document component
+ */
+class DocumentModelDownload extends DocumentModelDocument
+{
+
+
+}
+?>
